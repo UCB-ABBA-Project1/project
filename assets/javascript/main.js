972af4a8b3a20c84b803100c0167a4460b487033
Alex Camacho <alex.c@WalkMe.local>
2018-07-07 13:04:22 -0700

    fixed bug where subsequent queries didn't work, added pet-result class to each result div

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,8 +1,14 @@
 var petApiKey = '6ca77f8d1b56fdd653755579c78a336d';
 var petApiSecret = '65bb1fcddf5de94b4da39e27c387bf5e';
 
-var queryString = 'https://api.petfinder.com/pet.find?key=' + petApiKey;
-queryString += '&format=json'
+var queryString = '';
+
+var resetQueryString = function () {
+    queryString = 'https://api.petfinder.com/pet.find?key=' + petApiKey;
+    queryString += '&format=json'
+}
+
+resetQueryString();
 
 jQuery.ajaxPrefilter(function (options) {
     if (options.crossDomain && jQuery.support.cors) {
@@ -34,6 +40,50 @@ $('button[type=submit]').on("click", function (event) {
         url: queryString,
         method: "GET"
     }).then(function (response) {
-        console.log(response.petfinder.pets);
+        $("#results").html("");
+
+        console.log(response.petfinder.pets.pet);
+        var pets = response.petfinder.pets.pet;
+        pets.forEach(pet => {
+            var petDiv = $("<div>");
+            petDiv.addClass("pet-result");
+
+            var imgDiv = $("<img>");
+            var photosArray = pet.media.photos.photo;
+            photosArray.forEach(image => {
+                if (image["@size"] == "x") {
+                    imgDiv.attr("src", image.$t);
+                }
+            });
+            petDiv.append(imgDiv);
+
+            var name = $("<h4>");
+            name.text(pet.name.$t);
+            petDiv.append(name);
+
+            var breed = $("<p>");
+            breed.text("Breed: " + pet.breeds.breed.$t);
+            petDiv.append(breed);
+
+            var gender = $("<p>");
+            gender.text("Gender: " + pet.sex.$t);
+            petDiv.append(gender);
+
+            var ageType = $("<p>");
+            ageType.text("Age: " + pet.age.$t);
+            petDiv.append(ageType);
+
+            var description = $("<p>");
+            description.text(pet.description.$t);
+            petDiv.append(description);
+
+            var phone = $("<p>");
+            phone.text("Phone contact: " + pet.contact.phone.$t);
+            petDiv.append(phone);
+
+            $("#results").append(petDiv);
+
+            resetQueryString();
+        });
     })
 })
