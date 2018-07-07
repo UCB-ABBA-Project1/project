4d58d8a4ba36e54d9b3c297315fa3398351c16cd
Alex Camacho <alex.c@WalkMe.local>
2018-07-07 11:19:19 -0700

    Added ajax request for pet info, doesn't complete yet due to CORS

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,26 @@
-var test = 0;
+var petApiKey = '6ca77f8d1b56fdd653755579c78a336d';
+var petApiSecret = '65bb1fcddf5de94b4da39e27c387bf5e';
+
+var queryString = 'https://api.petfinder.com/pet.find?key=' + petApiKey;
+queryString += '&format=json'
+
+$('button[type=submit]').on("click", function (event) {
+    event.preventDefault();
+
+    var type = $('#animalType').val().trim();
+    var age = $('#ageType').val().trim();
+    var sex = $('#sexType').val().trim();
+    var zip = $('#inputZip').val().trim();
+
+    if (type !== '') queryString += '&animal=' + type;
+    if (age !== '') queryString += '&age=' + age;
+    if (sex !== '') queryString += '&sex=' + sex;
+    if (zip !== '') queryString += '&location=' + zip;
+
+    $.ajax({
+        url: queryString,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+    })
+})
